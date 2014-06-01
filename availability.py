4feabf29fb0892753a4c020355f2a3501ef1a0a2
tedder <ted@perljam.net>
2014-06-01 05:12:48 +0000

    handle failing server request silently

--- availability.py
+++ availability.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 
 import requests
+import sys
 
 page = requests.get('https://ws.ovh.com/dedicated/r2/ws.dispatcher/getAvailability2')
 data = page.json()
+
+# silently fail if the data is bad
+if not data:
+  sys.exit()
 
 sk1 = [x for x in data['answer']['availability'] if x['reference'] == '142sk1'][0]
 bhs = [x for x in sk1['zones'] if x['zone'] == 'bhs'][0]
